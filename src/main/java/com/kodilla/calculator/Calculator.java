d54879c4ff18975628788887431ffd650c4e7b4d
Bartosz Pawlik <merior94@gmail.com>
2022-03-03 20:04:28 +0100

    Calculator

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -1,2 +1,20 @@
-package com.kodilla.calculator;public class Calculator {
+package com.kodilla.calculator;
+
+public class Calculator {
+
+    public double add(double a, double b){
+        System.out.println(a+b);
+        return a+b;
+    }
+
+    public double substract(double a, double b){
+        System.out.println(a-b);
+        return a-b;
+    }
+
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        calculator.add(5,7);
+        calculator.substract(5,7);
+    }
 }
